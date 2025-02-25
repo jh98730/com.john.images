ff5442a415c1404e70563b616962920d432c8b63
John Howard <john.k.howard@gmail.com>
2025-02-25 16:23:26 -0500

    Initial Commit

---
+++ MyImages/MyImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyImagesApp.swift
+//  MyImages
+//
+//  Created by John Howard on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
